babe0908a2c13cadfaf3e191de29545ce38d3153
Hemanth Kalyan N Hegade <116051520+HemanthKalyanNHegade@users.noreply.github.com>
2024-01-22 21:09:47 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import colors from "colors";
+import path from "path";
+
+import noteRoutes from "./routes/noteRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import cors from "cors"
+
+dotenv.config();
+
+connectDB();
+
+const app = express(); // main thing
+
+app.use(cors())
+
+app.use(express.json()); // to accept json data
+
+app.use("/api/notes", noteRoutes);
+app.use("/api/users", userRoutes);
+
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow
+      .bold
+  )
+);
